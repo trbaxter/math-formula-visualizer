513d2cbe5aa7aa93990c2d1f91d5dc28edfb4df3
trbaxter <trb7074@gmail.com>
2024-05-12 01:09:31 -0500

    Added remaining project files

---
+++ src/test/java/com/math/formulavisualizer/MathFormulaVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math.formulavisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathFormulaVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
